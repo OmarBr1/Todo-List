a4467e8da345962241ace5d6e2bddef38ba014ab
OmarBr1 <omar.bourkab1@gmail.com>
2024-11-26 00:45:57 +0300

    My finale styles and updated code for the App

--- vite.config.js
+++ vite.config.js
@@ -5,15 +5,3 @@ export default defineConfig({
   base: '/Todo-List/',
   plugins: [react()],
 });
-
-
-
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-
-
